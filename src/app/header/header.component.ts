de6526a14bf43497e0ea4721b51d8b70f5698563
Sagar  Choudhari <sagarchoudhari909@gmail.com>
2024-02-24 12:13:53 +0530

    agriculture header file

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  navItems = [
+    { label: 'Home', href: '../', isActive: true },
+    { label: 'Pages', href: '#' },
+    { label: 'Blog', href: 'grid-blog.html' },
+    { label: 'Gallery', href: 'grid-gallery.html' },
+    { label: 'Elements', href: '#' },
+    { label: 'Shop', href: 'grid-shop.html' }
+  ];
+}
