5742a5c6038ea73ed6ac3b11463227a5a1e50e1e
akshaybhagwat76 <omkardhapte99@gmail.com>
2024-04-17 23:07:46 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,21 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
-import { ServicesComponent } from './services/services.component';
-import { FlavoursComponent } from './flavours/flavours.component';
 import { CounterComponent } from './counter/counter.component';
-import { ProductsComponent } from './products/products.component';
-import { PeopleComponent } from './people/people.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { TeamComponent } from './team/team.component';
-import { NewsletterComponent } from './newsletter/newsletter.component';
+import { FlavoursComponent } from './flavours/flavours.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { PeopleComponent } from './people/people.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
+import { TeamComponent } from './team/team.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent, ServicesComponent, FlavoursComponent, CounterComponent, ProductsComponent, PeopleComponent, GalleryComponent, TeamComponent, NewsletterComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent, ServicesComponent, FlavoursComponent, CounterComponent, ProductsComponent, PeopleComponent, TeamComponent, NewsletterComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
